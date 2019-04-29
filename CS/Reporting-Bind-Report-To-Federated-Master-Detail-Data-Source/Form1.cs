59543b7762c618d250c490f727dc65d2efa28d55
Andrey Lepikhov (auto) <andrey.lepikhov@gmail.com>
2019-04-29 16:46:50 +0300

    auto sync for version 2019.1

--- CS/Reporting-Bind-Report-To-Federated-Master-Detail-Data-Source/Form1.cs
+++ CS/Reporting-Bind-Report-To-Federated-Master-Detail-Data-Source/Form1.cs
@@ -1,88 +1,99 @@
-using DevExpress.DataAccess.ConnectionParameters;
-using DevExpress.DataAccess.DataFederation;
-using DevExpress.DataAccess.Excel;
-using DevExpress.DataAccess.Sql;
-using DevExpress.XtraReports.Configuration;
-using DevExpress.XtraReports.UI;
 using System;
 using System.ComponentModel;
 using System.Drawing;
 using System.Windows.Forms;
+using DevExpress.DataAccess.ConnectionParameters;
+using DevExpress.DataAccess.Sql;
+using DevExpress.DataAccess.Excel;
+using DevExpress.DataAccess.DataFederation;
+using DevExpress.XtraReports.UI;
 
 namespace BindReportToFederatedMasterDetailDataSource {
-    public partial class Form1 : Form {
-        public Form1() {
-            InitializeComponent();
-        }
-        void Button1_Click(object sender, EventArgs e) {
-            ReportDesignTool designTool = new ReportDesignTool(CreateReport());
-            designTool.ShowRibbonDesignerDialog();
-        }
+	public partial class Form1 : Form {
+		public Form1() {
+			InitializeComponent();
+		}
+		void Button1_Click(object sender, EventArgs e) {
+			ReportDesignTool designTool = new ReportDesignTool(CreateReport());
+			designTool.ShowRibbonDesignerDialog();
+		}
 
-        static FederationDataSource CreateFederationDataSource(SqlDataSource sql, ExcelDataSource excel) {
-            Source sqlSource = new Source(sql.Name, sql, "Categories");
-            Source excelSource = new Source(excel.Name, excel, "");
-            // Select the required columns from 
-            //the Sql Source for the Federation query result
-            var categoriesNode = sqlSource.From()
-                .Select("CategoryID", "CategoryName", "Description").Build("Categories");
-            // Select the required columns 
-            //from the Excel Source for the Federation query result
-            var productsNode = excelSource.From()
-                .Select("ProductName", "CategoryID", "UnitPrice").Build("Products");
+		static FederationDataSource CreateFederationDataSource(SqlDataSource sql, ExcelDataSource excel) {
+			// Create SQL and Excel sources.
+			Source sqlSource = new Source(sql.Name, sql, "Categories");
+			Source excelSource = new Source(excel.Name, excel, "");
 
-            var federationDataSource = new FederationDataSource();
-            federationDataSource.Queries.AddRange(new[] { categoriesNode, productsNode });
-            var relation = new FederationMasterDetailInfo("Categories", "Products", new FederationRelationColumnInfo("CategoryID", "CategoryID"));
-            federationDataSource.Relations.Add(relation);
-            federationDataSource.RebuildResultSchema();
-            return federationDataSource;
-        }
+			// Create the federated "Categores" query and select data from the SQL source's query.
+			var categoriesNode = sqlSource.From()
+				.Select("CategoryID", "CategoryName", "Description").Build("Categories");
+			// Create the federated "Products" query and select data from the Excel data source.
+			var productsNode = excelSource.From()
+				.Select("ProductName", "CategoryID", "UnitPrice").Build("Products");
 
-        public static XtraReport CreateReport() {
-            var report = new XtraReport();
-            var detailBand = new DetailBand() { HeightF = 25 };
-            var detailReport = new DetailReportBand();
-            var detailBand2 = new DetailBand() { HeightF = 25 };
-            detailReport.Bands.Add(detailBand2);
-            report.Bands.AddRange(new Band[] { detailBand, detailReport });
+			// Create a federated data source and add the federated queries to the collection.
+			var federationDataSource = new FederationDataSource();
+			federationDataSource.Queries.AddRange(new[] { categoriesNode, productsNode });
+			// Specify a master-detail relationship between these queries based on the "CategoryID" key field.
+			var relation = new FederationMasterDetailInfo("Categories", "Products", new FederationRelationColumnInfo("CategoryID", "CategoryID"));
+			federationDataSource.Relations.Add(relation);
+			// Build the data source schema to display it in the Field List.
+			federationDataSource.RebuildResultSchema();
 
-            var categoryLabel = new XRLabel() { WidthF = 150 };
-            var productLabel = new XRLabel() { WidthF = 300, LocationF = new PointF(100, 0) };
-            categoryLabel.ExpressionBindings.Add(new ExpressionBinding("BeforePrint", "Text", "[CategoryName]"));
-            productLabel.ExpressionBindings.Add(new ExpressionBinding("BeforePrint", "Text", "[ProductName]"));
-            detailBand.Controls.Add(categoryLabel);
-            detailBand2.Controls.Add(productLabel);
+			return federationDataSource;
+		}
 
-            var sqlDataSource = CreateSqlDataSource();
-            var excelDataSource = CreateExcelDataSource();
-            var federationDataSource = CreateFederationDataSource(sqlDataSource, excelDataSource);
-            report.ComponentStorage.AddRange(new IComponent[] { sqlDataSource, excelDataSource, federationDataSource });
-            report.DataSource = federationDataSource;
-            report.DataMember = "Categories";
-            detailReport.DataSource = federationDataSource;
-            detailReport.DataMember = "Categories.CategoriesProducts";
+		public static XtraReport CreateReport() {
+			// Create a new report.
+			var report = new XtraReport();
 
-            return report;
-        }
+			// Add the Detail band to the master report and create a label bound to the "CategoryName" data field.
+			var detailBand = new DetailBand() { HeightF = 25 };
+			var categoryLabel = new XRLabel() { WidthF = 150 };
+			categoryLabel.ExpressionBindings.Add(new ExpressionBinding("BeforePrint", "Text", "[CategoryName]"));
+			detailBand.Controls.Add(categoryLabel);
 
-        static SqlDataSource CreateSqlDataSource() {
-            var connectionParameters = new Access97ConnectionParameters("Data/nwind.mdb", "", "");
-            var sqlDataSource = new SqlDataSource(connectionParameters) { Name = "Sql_Categories" };
-            var categoriesQuery = SelectQueryFluentBuilder.AddTable("Categories").SelectAllColumnsFromTable().Build("Categories");
-            sqlDataSource.Queries.Add(categoriesQuery);
-            sqlDataSource.RebuildResultSchema();
-            return sqlDataSource;
-        }
+			// Create the detail report and add the Detail band to it.
+			var detailReport = new DetailReportBand();
+			var detailBand2 = new DetailBand() { HeightF = 25 };
+			detailReport.Bands.Add(detailBand2);
+			report.Bands.AddRange(new Band[] { detailBand, detailReport });
+			// Add a label bound to the "ProductName" data field.
+			var productLabel = new XRLabel() { WidthF = 300, LocationF = new PointF(100, 0) };
+			productLabel.ExpressionBindings.Add(new ExpressionBinding("BeforePrint", "Text", "[ProductName]"));
+			detailBand2.Controls.Add(productLabel);
 
-        static ExcelDataSource CreateExcelDataSource() {
-            var excelDataSource = new ExcelDataSource() { Name = "Excel_Products" };
-            excelDataSource.FileName = "Data/Products.xlsx";
-            excelDataSource.SourceOptions = new ExcelSourceOptions() {
-                ImportSettings = new ExcelWorksheetSettings("Sheet"),
-            };
-            excelDataSource.RebuildResultSchema();
-            return excelDataSource;
-        }
-    }
+			// Create data sources. 
+			var sqlDataSource = CreateSqlDataSource();
+			var excelDataSource = CreateExcelDataSource();
+			var federationDataSource = CreateFederationDataSource(sqlDataSource, excelDataSource);
+			// Add all data sources to the report to avoid serialization issues. 
+			report.ComponentStorage.AddRange(new IComponent[] { sqlDataSource, excelDataSource, federationDataSource });
+			// Assign a federated data source to the report and its detail report.
+			report.DataSource = federationDataSource;
+			report.DataMember = "Categories";
+			detailReport.DataSource = federationDataSource;
+			detailReport.DataMember = "Categories.CategoriesProducts";
+
+			return report;
+		}
+
+		static SqlDataSource CreateSqlDataSource() {
+			var connectionParameters = new Access97ConnectionParameters("Data/nwind.mdb", "", "");
+			var sqlDataSource = new SqlDataSource(connectionParameters) { Name = "Sql_Categories" };
+			var categoriesQuery = SelectQueryFluentBuilder.AddTable("Categories").SelectAllColumnsFromTable().Build("Categories");
+			sqlDataSource.Queries.Add(categoriesQuery);
+			sqlDataSource.RebuildResultSchema();
+			return sqlDataSource;
+		}
+
+		static ExcelDataSource CreateExcelDataSource() {
+			var excelDataSource = new ExcelDataSource() { Name = "Excel_Products" };
+			excelDataSource.FileName = "Data/Products.xlsx";
+			excelDataSource.SourceOptions = new ExcelSourceOptions() {
+				ImportSettings = new ExcelWorksheetSettings("Sheet"),
+			};
+			excelDataSource.RebuildResultSchema();
+			return excelDataSource;
+		}
+	}
 }
--- VB/Reporting-Bind-Report-To-Federated-Master-Detail-Data-Source/Form1.vb
+++ VB/Reporting-Bind-Report-To-Federated-Master-Detail-Data-Source/Form1.vb
@@ -1,13 +1,12 @@
-﻿Imports DevExpress.DataAccess.ConnectionParameters
-Imports DevExpress.DataAccess.DataFederation
-Imports DevExpress.DataAccess.Excel
-Imports DevExpress.DataAccess.Sql
-Imports DevExpress.XtraReports.Configuration
-Imports DevExpress.XtraReports.UI
-Imports System
+﻿Imports System
 Imports System.ComponentModel
 Imports System.Drawing
 Imports System.Windows.Forms
+Imports DevExpress.DataAccess.ConnectionParameters
+Imports DevExpress.DataAccess.Sql
+Imports DevExpress.DataAccess.Excel
+Imports DevExpress.DataAccess.DataFederation
+Imports DevExpress.XtraReports.UI
 
 Namespace BindReportToFederatedMasterDetailDataSource
     Partial Public Class Form1
@@ -22,45 +21,57 @@ Namespace BindReportToFederatedMasterDetailDataSource
         End Sub
 
         Private Shared Function CreateFederationDataSource(ByVal sql As SqlDataSource, ByVal excel As ExcelDataSource) As FederationDataSource
+            ' Create SQL and Excel sources.
             Dim sqlSource As New Source(sql.Name, sql, "Categories")
             Dim excelSource As New Source(excel.Name, excel, "")
-            ' Select the required columns from 
-            'the Sql Source for the Federation query result
+
+            ' Create the federated "Categores" query and select data from the SQL source's query.
             Dim categoriesNode = sqlSource.From().Select("CategoryID", "CategoryName", "Description").Build("Categories")
-            ' Select the required columns 
-            'from the Excel Source for the Federation query result
+            ' Create the federated "Products" query and select data from the Excel data source.
             Dim productsNode = excelSource.From().Select("ProductName", "CategoryID", "UnitPrice").Build("Products")
 
+            ' Create a federated data source and add the federated queries to the collection.
             Dim federationDataSource = New FederationDataSource()
             federationDataSource.Queries.AddRange( { categoriesNode, productsNode })
+            ' Specify a master-detail relationship between these queries based on the "CategoryID" key field.
             Dim relation = New FederationMasterDetailInfo("Categories", "Products", New FederationRelationColumnInfo("CategoryID", "CategoryID"))
             federationDataSource.Relations.Add(relation)
+            ' Build the data source schema to display it in the Field List.
             federationDataSource.RebuildResultSchema()
+
             Return federationDataSource
         End Function
 
         Public Shared Function CreateReport() As XtraReport
+            ' Create a new report.
             Dim report = New XtraReport()
+
+            ' Add the Detail band to the master report and create a label bound to the "CategoryName" data field.
             Dim detailBand = New DetailBand() With {.HeightF = 25}
+            Dim categoryLabel = New XRLabel() With {.WidthF = 150}
+            categoryLabel.ExpressionBindings.Add(New ExpressionBinding("BeforePrint", "Text", "[CategoryName]"))
+            detailBand.Controls.Add(categoryLabel)
+
+            ' Create the detail report and add the Detail band to it.
             Dim detailReport = New DetailReportBand()
             Dim detailBand2 = New DetailBand() With {.HeightF = 25}
             detailReport.Bands.Add(detailBand2)
             report.Bands.AddRange(New Band() { detailBand, detailReport })
-
-            Dim categoryLabel = New XRLabel() With {.WidthF = 150}
+            ' Add a label bound to the "ProductName" data field.
             Dim productLabel = New XRLabel() With { _
                 .WidthF = 300, _
                 .LocationF = New PointF(100, 0) _
             }
-            categoryLabel.ExpressionBindings.Add(New ExpressionBinding("BeforePrint", "Text", "[CategoryName]"))
             productLabel.ExpressionBindings.Add(New ExpressionBinding("BeforePrint", "Text", "[ProductName]"))
-            detailBand.Controls.Add(categoryLabel)
             detailBand2.Controls.Add(productLabel)
 
+            ' Create data sources. 
             Dim sqlDataSource = CreateSqlDataSource()
             Dim excelDataSource = CreateExcelDataSource()
             Dim federationDataSource = CreateFederationDataSource(sqlDataSource, excelDataSource)
+            ' Add all data sources to the report to avoid serialization issues. 
             report.ComponentStorage.AddRange(New IComponent() { sqlDataSource, excelDataSource, federationDataSource })
+            ' Assign a federated data source to the report and its detail report.
             report.DataSource = federationDataSource
             report.DataMember = "Categories"
             detailReport.DataSource = federationDataSource
